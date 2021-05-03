ac11def4014c2f6cd0042307ac849e280bab69d6
Bai Li <bai.li@noaa.gov>
2021-05-03 10:56:57 -0400

    update ss_empirical_waa_input data

--- R/ss_empirical_waa_input.R
+++ R/ss_empirical_waa_input.R
@@ -1,6 +1,6 @@
-#' Stock Synthesis (SS) input data from the simple example.
+#' Stock Synthesis (SS) input data from the empirical_watage_and_age_selex example.
 #'
-#' A dataset containing the SS stock assessment inputs for a simple example.
+#' A dataset containing the SS stock assessment inputs.
 #'
 #' @format A data list with 5 lists of data:
 #' \describe{
@@ -8,8 +8,8 @@
 #'   \item{ss_wtatage}{input data for ss_wtatage}
 #'   \item{ss_control}{input data for ss_control}
 #'   \item{ss_data}{input data for ss_data}
-#'   \item{ss_projection}{input data for ss_projection}
+#'   \item{ss_forecast}{input data for ss_forecast}
 #'   ...
 #' }
-#' @source \url{https://vlab.ncep.noaa.gov/web/stock-synthesis/document-library/-/document_library/0LmuycloZeIt/view/11684751?_com_liferay_document_library_web_portlet_DLPortlet_INSTANCE_0LmuycloZeIt_redirect=https%3A%2F%2Fvlab.ncep.noaa.gov%3A443%2Fweb%2Fstock-synthesis%2Fdocument-library%2F-%2Fdocument_library%2F0LmuycloZeIt%2Fview%2F11684741}
+#' @source \url{https://github.com/nmfs-stock-synthesis/user-examples}
 "ss_empirical_waa_input"
