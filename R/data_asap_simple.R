a19ac3ee19901412d842aa6125bc3da2fcbc3b0d
Bai Li <bai.li@noaa.gov>
2021-08-23 09:31:35 -0400

    combine asap input and output to one data list

---
+++ R/data_asap_simple.R
@@ -0,0 +1,12 @@
+#' Age Structured Assessment Program (ASAP) input and output data from the simple example.
+#'
+#' A dataset containing the ASAP stock assessment input and output for a simple example.
+#'
+#' @format A data list with 2 lists of data:
+#' \describe{
+#'   \item{input}{ASAP stock assessment input data list. It includes 4 lists of data: dat, comments, fleet.names, and survey.names.}
+#'   \item{output}{ASAP stock assessment output data list. It includes 58 lists of data.}
+#'   ...
+#' }
+#' @source \url{https://nmfs-fish-tools.github.io/ASAP/}
+"asap_simple"
